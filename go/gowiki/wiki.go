fbf267ca72ed68e1fa31371cf22f5474d44a25d2
Rohit Yadav <rohityadav89@gmail.com>
2010-11-24 04:23:52 +0530

    My recent hack outs with go

---
+++ go/gowiki/wiki.go
@@ -0,0 +1,99 @@
+/* 
+ * Tutorial at: http://golang.org/doc/codelab/wiki/ 
+ * Run http://localhost:8080/view/<name of your wiki page>
+ */
+
+package main
+
+import (
+    "http"
+    "io/ioutil"
+    "os"
+    "regexp"
+    "template"
+)
+
+type page struct {
+    title string
+    body  []byte
+}
+
+func (p *page) save() os.Error {
+    filename := p.title + ".txt"
+    return ioutil.WriteFile(filename, p.body, 0600)
+}
+
+func loadPage(title string) (*page, os.Error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &page{title: title, body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+    p, err := loadPage(title)
+    if err != nil {
+        p = &page{title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+    body := r.FormValue("body")
+    p := &page{title: title, body: []byte(body)}
+    err := p.save()
+    if err != nil {
+        http.Error(w, err.String(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var templates = make(map[string]*template.Template)
+
+func init() {
+    for _, tmpl := range []string{"edit", "view"} {
+        templates[tmpl] = template.MustParseFile(tmpl+".html", nil)
+    }
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *page) {
+    err := templates[tmpl].Execute(p, w)
+    if err != nil {
+        http.Error(w, err.String(), http.StatusInternalServerError)
+    }
+}
+
+const lenPath = len("/view/")
+
+var titleValidator = regexp.MustCompile("^[a-zA-Z0-9]+$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        title := r.URL.Path[lenPath:]
+        if !titleValidator.MatchString(title) {
+            http.NotFound(w, r)
+            return
+        }
+        fn(w, r, title)
+    }
+}
+
+func main() {
+    http.HandleFunc("/view/", makeHandler(viewHandler))
+    http.HandleFunc("/edit/", makeHandler(editHandler))
+    http.HandleFunc("/save/", makeHandler(saveHandler))
+    http.ListenAndServe(":8080", nil)
+}
+
