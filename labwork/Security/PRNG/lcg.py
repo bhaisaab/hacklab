a75b30dc8990521a5bd58e450f11977ca6d36f8c
Rohit Yadav <rohityadav89@gmail.com>
2011-03-16 13:34:10 +0530

    Security: LCG PRNG

---
+++ labwork/Security/PRNG/lcg.py
@@ -0,0 +1,23 @@
+# Linear Congruential Generator
+# xi = (a * xi + x) mod m
+# period is atmost m, c!=0, iff:
+# 1. c & m are co-primes 
+# 2. a-1 is divisible by prime factors of m
+# 3. a-1 is multiple of 4 if m is multiple of 4
+
+a = 5
+c = 9
+m = 16
+xi = 0
+
+def seed(x):
+    global xi
+    xi = x
+
+def rng():
+    global xi
+    xi = (a*xi + c)%m
+    return xi
+
+for i in range(20):
+    print rng()
