42fb213a9fc94c4e03b390a6432dae0ae60b2b64
Rohit Yadav <rohityadav89@gmail.com>
2012-03-31 17:27:28 +0530

    euler: finally solved euler #372

--- coding/projecteuler/372.cpp
+++ coding/projecteuler/372.cpp
@@ -1,39 +1,36 @@
 //g++ -std=c++0x -pthread 372.cpp
+//301450082318807027
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <thread>
 
-#define SQLIM 300000
-
 using namespace std;
 
 mutex m;
 unsigned long long sum = 0ULL;
-unsigned long long minr = 2000000ULL;
-unsigned long long maxr = 1000000000ULL;
-double sqrts[SQLIM];
+unsigned long long minr = 2e6;
+unsigned long long maxr = 1e9;
 
 void
 setofy(unsigned long val)
 {
   unsigned long long lsum = 0ULL;
   unsigned long long x = minr+1;
-  unsigned long long lim = ceil(1.0*maxr/sqrt(val))+1;
-  if (lim>maxr) lim = maxr;
-  while (x<=lim)
+  long double t1 = sqrt(val);
+  long double t2 = sqrt(val+1);
+  long double s1, s2;
+  s1=x*t1;
+  s2=x*t2;
+  for(; x<=maxr; x++,s1+=t1,s2+=t2)
   {
-    printf("");
-    unsigned long long y2 = ceil(x*sqrts[val+1]);
-    unsigned long long y1 = floor(x*sqrts[val]);
+    unsigned long long y2 = s2+1;
+    unsigned long long y1 = s1-1;;
+    while( (y1*y1) < (x*x*val) ) y1++;
+    while( (y2*y2) >= (x*x*(val+1)) ) y2--;
     if(y1>maxr) break;
     if(y2>maxr) y2 = maxr;
-    while( (y1<=y2) && ( (unsigned long long)floor(1.0*y1*y1/(x*x)) != val) ) y1++;
-    while( (y1<y2) && ( (unsigned long long)floor(1.0*y2*y2/(x*x)) != val) ) y2--;
     if(y2>=y1) lsum+=(y2-y1+1);
-    //for(;y2>=y1;y1++)
-      //if((unsigned long long)floor(1.0*y1*y1/(x*x)) == val) lsum++;
-    x++;
   }
   m.lock();
   sum+=lsum;
@@ -42,16 +39,13 @@ setofy(unsigned long val)
 
 int main()
 {
- // printf("enter m, n: ");
+  //printf("enter m, n: ");
   //int i = scanf("%llu %llu", &minr, &maxr);
   printf("M = %llu, N = %llu, sum=%llu\n", minr, maxr, sum);
 
   unsigned long maxv = floor( (double)( 1.0 * maxr *  maxr / ((minr+1)*(minr+1)) ) );
   unsigned long val = 1ULL;
   unsigned long long x = minr+1;
-
-  for(int i=1; i<=(maxv+10); ++i)
-    sqrts[i] = sqrt(i);
 
   int parts = 8;
   thread* tt = new thread[parts];
