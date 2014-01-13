f17a3e63cce06f67b74065616433f6dfab9ef4b1
Rohit Yadav <bhaisaab@baagi.org>
2014-01-13 13:37:00 +0530

    usaco: milk2 in C89
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- usaco/milk2/milk2.c
+++ usaco/milk2/milk2.c
@@ -8,16 +8,60 @@ PROG: milk2
 #include <stdlib.h>
 #include <string.h>
 
+// took 40 mins to think, need to improve timings
 int main() {
     FILE* fin = fopen("milk2.in", "r");
     FILE* fout = fopen("milk2.out", "w");
 
     int n;
     int time[5000][2];
+    int minTime = 0;
+    int maxTime = 0;
+    int milkTime = 0;
+    int nonMilkTime = 0;
+    int i, j, start, stop;
 
     fscanf(fin, "%d\n", &n);
-    for (int i = 0; i < n; i++) //C99 loop
+    for (i = 0; i < n; i++) //C99 loop
         fscanf(fin, "%d %d\n", &time[i][0], &time[i][1]);
+
+    //sort time by time[0]
+    for (i = 0; i < n-1; i++) {
+        for (j = i+1; j < n; j++) {
+            if (time[j][0] < time[i][0]) {
+                start = time[i][0];
+                stop = time[i][1];
+                // swap i and j
+                time[i][0] = time[j][0];
+                time[i][1] = time[j][1];
+
+                time[j][0] = start;
+                time[j][1] = stop;
+            }
+        }
+    }
+
+    minTime = time[0][0];
+    maxTime = time[0][1];
+    milkTime = maxTime - minTime;
+    nonMilkTime = 0;
+    for (i = 1; i < n; i++) {
+        // no overlap
+        if (time[i][0] > maxTime) {
+            minTime = time[i][0];
+            if ((time[i][0]-maxTime) > nonMilkTime)
+                nonMilkTime = (time[i][0] - maxTime);
+            maxTime = time[i][1];
+        } else {
+            if (time[i][1] > maxTime) {
+                maxTime = time[i][1];
+            }
+        }
+        if ((maxTime - minTime) > milkTime)
+            milkTime = maxTime - minTime;
+    }
+
+    fprintf(fout, "%d %d\n", milkTime, nonMilkTime);
 
     fclose(fin);
     fclose(fout);
