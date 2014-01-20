372e60c2f6b8f9e906f67c52db950f5e7918c8ad
Rohit Yadav <bhaisaab@baagi.org>
2014-01-20 13:58:27 +0530

    usaco: palsquare stubs
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

---
+++ usaco/palsquare/palsquare.cpp
@@ -0,0 +1,16 @@
+/*
+ID: rohitya1
+LANG: C++11
+PROG: namenum
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
