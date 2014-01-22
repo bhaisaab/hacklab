ea8afce07dd76836a087cf4943960dd4e78c986f
Rohit Yadav <bhaisaab@baagi.org>
2014-01-22 13:56:59 +0530

    usaco: add dualpal's stubs
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

---
+++ usaco/dualpal/dualpal.cpp
@@ -0,0 +1,23 @@
+/*
+ID: rohitya1
+LANG: C++11
+PROG: dualpal
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ifstream fin("dualpal.in");
+    ofstream fout("dualpal.out");
+
+    fin.close();
+    fout.close();
+    return 0;
+}
