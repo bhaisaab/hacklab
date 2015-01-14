97af5f645f04ce3dfcc1d474ec5ca5334fdf1058
Rohit Yadav <bhaisaab@baagi.org>
2015-01-15 04:18:02 +0530

    spoj: LASTDIG optimized
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- spoj/3442.go
+++ spoj/3442.go
@@ -3,20 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
-	cheat := [10][]int{
-		[]int{0},
-		[]int{1},
-		[]int{2, 4, 8, 6},
-		[]int{3, 9, 7, 1},
-		[]int{4, 6},
-		[]int{5},
-		[]int{6},
-		[]int{7, 9, 3, 1},
-		[]int{8, 4, 2, 6},
-		[]int{9, 1},
-	}
-
 	var t int
 	fmt.Scanf("%d", &t)
 	for ; t > 0; t-- {
@@ -26,9 +12,17 @@ func main() {
 			fmt.Println(1)
 			continue
 		}
-		digit := base % 10
-		l := len(cheat[digit])
-		idx := (index - 1) % l
-		fmt.Println(cheat[digit][idx])
+
+		//right to left binary method
+		result := 1
+		base = base % 10
+		for index > 0 {
+			if index%2 != 0 {
+				result = (result * base) % 10
+			}
+			base = (base * base) % 10
+			index >>= 1
+		}
+		fmt.Println(result)
 	}
 }
