4e7b78965c78c8c16f8670ded7d7f1856eec6edf
Rohit Yadav <bhaisaab@baagi.org>
2016-11-20 13:16:48 +0530

    python: complications with decorations
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- python/dynamic-import/main.py
+++ python/dynamic-import/main.py
@@ -1,5 +1,26 @@
 import sys
 
+def dimport(name): # module name could be a, b or something like a.b.c.d etc.
+    def wrapper(func):
+        try:
+            mod = __import__(name)
+            mod.run()
+        except ImportError: pass
+        # log error?
+        return func
+    return wrapper
+
+
+@dimport("a")
+def decorator_maina():
+    print("Method with A run first")
+
+
+@dimport("b")
+def decorator_mainb():
+    print("Method with B run first")
+
+
 def main():
     if len(sys.argv) > 1:
         modname = sys.argv[-1]
@@ -8,6 +29,10 @@ def main():
             mod.run()
         except ImportError:
             print("No valid module found for the name: ", modname, "Failed to import module")
+
+    print("\nNow with @decorators:")
+    decorator_maina()
+    decorator_mainb()
 
 if __name__ == "__main__":
     main()
